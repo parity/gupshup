1e866dae0da00feee981764466e7bbb6edae50d0
Beena Shetty <binu.shetty@gmail.com>
2015-04-13 10:36:31 +0530

    Added test cases

--- gupshup.gemspec
+++ gupshup.gemspec
@@ -2,16 +2,16 @@
 
 Gem::Specification.new do |s|
   s.name = %q{gupshup}
-  s.version = "0.3.0"
+  s.version = "1.0.0"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
-  s.authors = ["Nilesh Trivedi"]
+  s.authors = ["Beena Shetty"]
   s.date = %q{2015-03-04}
   s.description = %q{Ruby wrapper for SMSGupShup API}
-  s.email = ["nilesh.tr@gmail.com"]
+  s.email = ["binu.shetty@gmail.com"]
   s.extra_rdoc_files = ["History.txt", "Manifest.txt", "PostInstall.txt"]
   s.files = ["History.txt", "Manifest.txt", "PostInstall.txt", "README.rdoc", "Rakefile", "lib/gupshup.rb", "script/console", "script/destroy", "script/generate", "test/test_gupshup.rb", "test/test_helper.rb"]
-  s.homepage = %q{http://github.com/nileshtrivedi/gupshup}
+  s.homepage = %q{http://github.com/parity/gupshup}
   s.post_install_message = %q{PostInstall.txt}
   #s.rdoc_options = ["--main", "README.rdoc"]
   s.require_paths = ["lib"]
--- test/test_gupshup.rb
+++ test/test_gupshup.rb
@@ -1,11 +1,37 @@
-require File.dirname(__FILE__) + '/test_helper.rb'
-
+$:.unshift File.dirname(__FILE__)
+require 'test_helper'
 class TestGupshup < Test::Unit::TestCase
 
   def setup
+  	@gup = Gupshup::Enterprise.new(:userid => '19009888', :password => 'test')
   end
   
   def test_truth
-    assert true
+    assert_equal([true, nil], @gup.send_text_message(msg: "Testing", send_to: "919453456689"))
   end
+
+  def test_failure
+    assert_not_equal([true, nil], @gup.send_text_message(msg: "Testing", send_to: "4567777888"))
+  end
+
+  def test_creditional
+  	assert_raise( RuntimeError ){Gupshup::Enterprise.new(userid: "")}
+  end
+
+  def test_short_phone_number
+  	assert_equal([false, "Phone Number is too short"], @gup.send_text_message(msg: "Testing", send_to: "9194567890"))
+  end
+
+  def test_long_phone_number
+  	assert_equal([false, "Phone Number is too long"], @gup.send_text_message(msg: "Testing", send_to: "91945678901123"))
+  end
+  
+  def test_string
+  	assert_equal([false, "Phone Number should be numerical value"], @gup.send_text_message(msg: "Testing", send_to: "hellotest123"))
+  end
+
+  def test_empty_msg
+  	assert_equal([false, "Message cannot be empty"], @gup.send_text_message(msg: "", send_to: "919453456689"))
+  end
+
 end
